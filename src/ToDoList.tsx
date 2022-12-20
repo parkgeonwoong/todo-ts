bd2faf01f1ca207d2ac5c459acd66853aeb56fde
parkgeonwoong <hk77kh@gmail.com>
2022-12-20 14:16:56 +0900

    Feat : react-hook-form 바꿔보기

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,11 +1,14 @@
 /**
  * @desc : TodoList
+ * @todo :
+ * 1. react-hook-form 사용하기
  *
  */
 
 import React, { useState } from "react";
+import { useForm } from "react-hook-form";
 
-function ToDoList() {
+/* function ToDoList() {
   const [toDo, setToDo] = useState("");
   const onChange = (event: React.FormEvent<HTMLInputElement>) => {
     const {
@@ -23,6 +26,22 @@ function ToDoList() {
     <div>
       <form onSubmit={onSubmit}>
         <input onChange={onChange} value={toDo} placeholder="Write to do" />
+        <button>Add</button>
+      </form>
+    </div>
+  );
+} */
+
+function ToDoList() {
+  const { register, watch } = useForm();
+  console.log("toDo");
+  console.log(watch());
+
+  return (
+    <div>
+      <form>
+        {/* useForm 사용 */}
+        <input {...register("toDo")} placeholder="Write to do" />
         <button>Add</button>
       </form>
     </div>
