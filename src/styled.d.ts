9ca41d8893b5a84bccd17c27f9d89d41be5800fe
parkgeonwoong <hk77kh@gmail.com>
2022-12-20 10:52:21 +0900

    Feat : Todo form 세팅

--- src/styled.d.ts
+++ src/styled.d.ts
@@ -1,5 +1,12 @@
+/**
+ * @desc : theme 테마 타입 지정
+ */
+
 import "styled-components";
 
 declare module "styled-components" {
-  export interface DefaultTheme {}
+  export interface DefaultTheme {
+    bgColor: string;
+    textColor: string;
+  }
 }
---
+++ src/theme.ts
@@ -0,0 +1,10 @@
+/**
+ * @desc : 테마 지정 index.tsx에서 props
+ */
+
+import { DefaultTheme } from "styled-components";
+
+export const theme: DefaultTheme = {
+  textColor: "#2f3640",
+  bgColor: "#f5f6fa",
+};
